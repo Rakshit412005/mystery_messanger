b88d94940e0b385611d95c8278ef6111f7e42441
Rakshit412005 <rk1056@srmist.edu.in>
2025-11-10 01:30:59 +0530

    ..

--- src/model/User.ts
+++ src/model/User.ts
@@ -6,5 +6,40 @@ export interface Message extends Document{
 }
 
 const MessageSchema : Schema <Message> = new Schema({
-    
+    content:{
+        type:String,
+        required: true
+    },
+    createdAt:{
+        type:Date,
+        required: true,
+        default: Date.now
+    }
 })
+
+export interface User extends Document{
+    username : string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    message: Message[]
+}
+ const UserSchema :Schema<User> = new Schema({
+    username:{
+        type:String,
+        required:[true,"username is required"],
+        trim:true,
+        unique:true
+    }
+    email:{
+        type : String,
+        required: true,
+        unique: true,
+        match:[,'please use a valid email address']
+    },
+    verifyCode:{
+        required: [true,"verify code is required"],
+    }
+ })
